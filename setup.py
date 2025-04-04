edff410efc2c57f030ca2a2afba753c56e065470
Shaikh Abdul Mannan <abdulmannan8334@gmail.com>
2025-04-05 01:05:21 +0530

    fix: add flask-cors to requirements and setup

--- setup.py
+++ setup.py
@@ -1,20 +1,21 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='plant-disease-detection',
-    version='0.1.0',
+    name="plant-disease-detection",
+    version="0.1.0",
     packages=find_packages(),
     install_requires=[
-        'torch',
-        'torchvision',
-        'torchaudio',
-        'pycocotools',
-        'roboflow',
-        'Flask',
-        'ultralytics',
+        "torch",
+        "torchvision",
+        "torchaudio",
+        "pycocotools",
+        "roboflow",
+        "Flask",
+        "ultralytics",
+        "flask-cors",
     ],
     entry_points={
-        'console_scripts': [
+        "console_scripts": [
             # Add your command line scripts here
         ],
     },
